1eb26bae11e2d0eccfb3210245a76fd17fb109e8
Lane Wiley <lanewiley@zohomail.com>
2021-08-18 15:25:23 -0400

    Removed unused imports

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,7 +1,4 @@
-import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
-import Hidden from '@material-ui/core/Hidden';
-import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/core/styles';
